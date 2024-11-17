667309e630280ffce64127e74f624288636bed60
eampatin <emmanuelampatin37@gmail.com>
2024-11-17 21:02:13 +0800

    add dashboard and nd logout functionality

--- src/views/system/DashboardView.vue
+++ src/views/system/DashboardView.vue
@@ -1,76 +1,40 @@
-<template>
-  <v-card class="mx-auto" color="surface-light" max-width="600">
-    <template v-slot:prepend>
-      <v-icon
-        :color="checking ? 'red lighten-2' : 'indigo-lighten-2'"
-        class="me-8"
-        icon="mdi-heart-pulse"
-        size="64"
-        @click="takePulse"
-      ></v-icon>
-    </template>
-
-    <template v-slot:title>
-      <div class="text-caption text-grey text-uppercase">Heart rate</div>
-
-      <span class="text-h3 font-weight-black" v-text="avg || '—'"></span>
-      <strong v-if="avg">BPM</strong>
-    </template>
-
-    <template v-slot:append>
-      <v-btn class="align-self-start" icon="mdi-arrow-right-thick" size="34" variant="text"></v-btn>
-    </template>
-
-    <v-sheet color="transparent">
-      <v-sparkline
-        :key="String(avg)"
-        :gradient="['#f72047', '#ffd200', '#1feaea']"
-        :line-width="3"
-        :model-value="heartbeats"
-        :smooth="16"
-        stroke-linecap="round"
-        auto-draw
-      ></v-sparkline>
-    </v-sheet>
-  </v-card>
-</template>
 <script>
-const exhale = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { supabase } from '../../utils/supabase.js'
 
 export default {
-  data: () => ({
-    checking: false,
-    heartbeats: []
-  }),
+  setup() {
+    const router = useRouter()
+    const formAction = ref({
+      formProcess: false
+    })
 
-  computed: {
-    avg() {
-      const sum = this.heartbeats.reduce((acc, cur) => acc + cur, 0)
-      const length = this.heartbeats.length
+    const onLogout = async () => {
+      formAction.value = { formProcess: true }
 
-      if (!sum && !length) return 0
+      const { error } = await supabase.auth.signOut()
+      if (error) {
+        console.error('Error during logout:', error)
+        return
+      }
 
-      return Math.ceil(sum / length)
+      formAction.value.formProcess = false
+      router.replace('/login')
     }
-  },
 
-  created() {
-    this.takePulse(false)
-  },
-
-  methods: {
-    heartbeat() {
-      return Math.ceil(Math.random() * (120 - 80) + 80)
-    },
-    async takePulse(inhale = true) {
-      this.checking = true
-
-      inhale && (await exhale(1000))
-
-      this.heartbeats = Array.from({ length: 20 }, this.heartbeat)
-
-      this.checking = false
+    return {
+      formAction,
+      onLogout
     }
   }
 }
 </script>
+
+<template>
+  <v-card class="mx-auto" color="surface-light" max-width="600">
+    <template v-slot:append>
+      <v-btn class="align-self-start" color="primary" @click="onLogout">Logout</v-btn>
+    </template>
+  </v-card>
+</template>
