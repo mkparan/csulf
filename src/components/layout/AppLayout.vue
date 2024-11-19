9f980af0a7ccc91f7b16370cbc7497fa9a5671f9
mkparan <mdspot13@gmail.com>
2024-11-20 00:55:28 +0800

    updated ui and layout for login and reg

---
+++ src/components/layout/AppLayout.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from 'vue'
+
+const theme = ref('light')
+</script>
+
+<template>
+  <v-responsive class="border rounded">
+    <v-app :theme="theme">
+      <v-main class="d-flex align-center justify-center fill-height">
+        <v-container>
+          <slot name="content"></slot>
+        </v-container>
+      </v-main>
+      <v-footer
+        border
+        app
+        class="bg-light-green-darken-3 text-center d-flex flex-column"
+      >
+        <div>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            :icon="icon"
+            class="mx-4"
+            variant="text"
+          ></v-btn>
+        </div>
+
+        <div class="pt-0">Caraga State University - Lost and Found</div>
+
+        <v-divider></v-divider>
+
+        <div>
+          {{ new Date().getFullYear() }} —
+          <strong>All Rights Reserved</strong>
+        </div>
+      </v-footer>
+    </v-app>
+  </v-responsive>
+</template>
+
+<style scoped>
+.fill-height {
+  min-height: 100vh; /* Ensures container takes the full viewport height */
+}
+</style>
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,102 +1,88 @@
 <script setup>
-import { ref } from 'vue'
+import AppLayout from '@/components/layout/AppLayout.vue'
+import { useDisplay } from 'vuetify'
 
-const theme = ref('light')
+const { mobile } = useDisplay()
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <v-main>
-        <v-container class="fill-height" fluid>
-          <v-row class="d-flex align-center justify-center">
-            <v-col cols="12" md="6" lg="4" class="mx-auto">
-              <v-card
+  <AppLayout>
+    <template #content>
+      <v-row class="d-flex align-center justify-center">
+        <v-col cols="12" md="6" class="mx-auto pt-16">
+          <v-card class="mx-auto" elevation="16">
+            <v-card-title class="text-center">
+              <v-img
                 class="mx-auto"
-                prepend-icon="mdi-account-plus"
-                subtitle="Register"
-              >
-                <template v-slot:title>
-                  <span class="font-weight-black">Welcome to Vuetify</span>
-                </template>
+                src="/images/logo.png"
+                :width="mobile ? '60%' : '40%'"
+              ></v-img>
+              <h3 class="font-weight-black">WELCOME</h3>
+            </v-card-title>
 
-                <v-card-text class="bg-surface-light pt-4">
-                  <v-form fast-fail @submit.prevent>
-                    <v-text-field
-                      rounded
-                      label="Firstname"
-                      variant="solo-filled"
-                    ></v-text-field>
+            <v-card-text class="bg-surface-light pt-4">
+              <v-divider class="my-5"></v-divider>
 
-                    <v-text-field
-                      rounded
-                      label="Lastname"
-                      variant="solo-filled"
-                    ></v-text-field>
+              <v-form fast-fail @submit.prevent>
+                <v-text-field
+                  rounded
+                  label="Firstname"
+                  variant="solo-filled"
+                  color="green-darken-3"
+                ></v-text-field>
 
-                    <v-text-field
-                      rounded
-                      label="Email"
-                      variant="solo-filled"
-                    ></v-text-field>
+                <v-text-field
+                  rounded
+                  label="Lastname"
+                  variant="solo-filled"
+                  color="green-darken-3"
+                ></v-text-field>
 
-                    <v-text-field
-                      rounded
-                      label="Password"
-                      type="password"
-                      variant="solo-filled"
-                    ></v-text-field>
+                <v-text-field
+                  rounded
+                  label="Email"
+                  variant="solo-filled"
+                  color="green-darken-3"
+                ></v-text-field>
 
-                    <v-text-field
-                      rounded
-                      label="Confirm Password"
-                      type="password"
-                      variant="solo-filled"
-                    ></v-text-field>
+                <v-text-field
+                  rounded
+                  label="Password"
+                  type="password"
+                  variant="solo-filled"
+                  color="green-darken-3"
+                ></v-text-field>
 
-                    <v-btn rounded class="mt-2" type="submit" block
-                      >Submit</v-btn
-                    >
-                  </v-form>
+                <v-text-field
+                  rounded
+                  label="Confirm Password"
+                  type="password"
+                  variant="solo-filled"
+                  color="green-darken-3"
+                ></v-text-field>
 
-                  <v-divider class="my-5"></v-divider>
+                <v-btn
+                  rounded
+                  class="mt-2"
+                  type="submit"
+                  block
+                  color="green-darken-3"
+                  >Register</v-btn
+                >
+              </v-form>
 
-                  <h5 class="text-center">
-                    Already have an account?
-                    <RouterLink class="text-primary" to="/">
-                      Login here!</RouterLink
-                    >
-                  </h5>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-      <v-footer
-        border
-        app
-        class="bg-light-green-darken-3 text-center d-flex flex-column"
-      >
-        <div>
-          <v-btn
-            v-for="icon in icons"
-            :key="icon"
-            :icon="icon"
-            class="mx-4"
-            variant="text"
-          ></v-btn>
-        </div>
+              <v-divider class="my-5"></v-divider>
 
-        <div class="pt-0">Caraga State University - Lost and Found</div>
-
-        <v-divider></v-divider>
-
-        <div>
-          {{ new Date().getFullYear() }} —
-          <strong>AMPATIN - IGNACIO - PARAN</strong>
-        </div>
-      </v-footer>
-    </v-app>
-  </v-responsive>
+              <h5 class="text-center">
+                Already have an account?
+                <RouterLink class="text-primary" to="/">
+                  Login here!</RouterLink
+                >
+              </h5>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row></template
+    >
+  </AppLayout>
 </template>
